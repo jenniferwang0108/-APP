d5c9c64b62ddbaa75e1317fb0ebcc7ca42e7b6fc
Jennifer Wang <jenniferwang@Jennifers-MacBook-Air-3.local>
2017-03-19 23:11:35 +0800

    3/19

---
+++ 給我一首歌的時間/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  給我一首歌的時間
+//
+//  Created by Jennifer Wang on 3/18/17.
+//  Copyright © 2017 Jennifer Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
